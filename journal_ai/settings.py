cb22557e2eac43863dee957263c8ae126cd2b1dd
Guzzler <sharangpai123@gmail.com>
2023-01-23 16:34:34 -0500

    connect postgres with django (basic setup)

--- journal_ai/settings.py
+++ journal_ai/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_browser_reload",
+    "journal_ai"
 ]
 
 MIDDLEWARE = [
@@ -81,12 +82,16 @@ WSGI_APPLICATION = "journal_ai.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'), 
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'), 
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 
 
 # Password validation
