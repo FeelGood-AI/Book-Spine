e390082812bd0a5e4bbf06b966edf2eab672334a
Guzzler <sharangpai123@gmail.com>
2023-01-26 16:54:16 -0500

    add permission for test view to show proper usage

--- journal_ai/core/views.py
+++ journal_ai/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from journal_ai.models import Test
 from journal_ai.core.serializers import TestSerializer
 
@@ -14,8 +15,10 @@ def index(request):
     )
 
 
-# TEST VIEW meant to illustrate how get view are written 
+# TEST VIEW meant to illustrate how get view are written
+# Example of permission decorator usage 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getTest(request):
     food = Test.objects.all()
     serializer = TestSerializer(food, many=True)
