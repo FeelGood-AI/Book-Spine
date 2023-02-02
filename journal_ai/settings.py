202f84068c9ca48aae1fe4e7f5232bddd8526481
Guzzler <sharangpai123@gmail.com>
2023-02-02 00:30:49 -0500

    added allowed host changes

--- journal_ai/settings.py
+++ journal_ai/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
 
 
 # Application definition
@@ -144,6 +144,8 @@ REST_FRAMEWORK = {
 }
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
