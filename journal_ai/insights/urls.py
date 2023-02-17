7527aabe6c5c5d0a99b3617ee56d046ca3417ec9
Guzzler <sharangpai123@gmail.com>
2023-02-17 01:26:23 -0500

    fix url order to fix bug

--- journal_ai/insights/urls.py
+++ journal_ai/insights/urls.py
@@ -4,7 +4,7 @@ from .views import InsightView, getInsightByMemoir,MarkInsightHelpful,MarkInsigh
 urlpatterns = [
     path('', InsightView.as_view(), name='insightView'),
     path('<uuid:memoir_id>', getInsightByMemoir, name='getInsightByMemoir'),
-    path('<str:auth_key>/<uuid:memoir_id>', putInsightIntoMemoir, name='putInsightIntoMemoir'),
     path('mark_helpful/<uuid:insight_id>', MarkInsightHelpful.as_view(), name='markInsightHelpful'),
-    path('mark_read/<uuid:insight_id>', MarkInsightRead.as_view(), name='markInsightHelpful')
+    path('mark_read/<uuid:insight_id>', MarkInsightRead.as_view(), name='markInsightHelpful'),
+    path('<str:auth_key>/<uuid:memoir_id>', putInsightIntoMemoir, name='putInsightIntoMemoir'),
 ] 
