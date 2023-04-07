58ba12ac7174c9d9621fbe11b1a971610d04f61d
Marlies Goes <marlies.goes@icloud.com>
2023-04-07 15:41:56 -0400

    add debug mode (hardcoded insight)

--- journal_ai/memoirs/tasks.py
+++ journal_ai/memoirs/tasks.py
@@ -11,31 +11,42 @@ from ..insights.serializers import InsightSerializer
 from django.utils import timezone
 
 BASE_ENDPOINT = os.environ.get('BASE_ENDPOINT')
+DEBUG = os.environ.get('DEBUG')
 
 logger = get_task_logger(__name__)
 
 # create a shared task to call an external API for getting insight given prompt and journal entry
+
+
 @shared_task
 def get_insight(journaler_id, memoir_id):
     user = User.objects.filter(pk=journaler_id).first()
     if not user:
-        logger.info('user not found for journaler id: {0}'.format(journaler_id))
+        logger.info(
+            'user not found for journaler id: {0}'.format(journaler_id))
         return
     memoir = Memoir.objects.filter(pk=memoir_id).first()
     jprompt = memoir.getPrompt()
     logger.info(f"jprompt: {jprompt.keys()}")
     jprompt = jprompt['text']
     entry = memoir.text
-    url = f"{BASE_ENDPOINT}/get_insight?entry={entry}&jprompt={jprompt}"
-    response = requests.request("GET", url)
+    if DEBUG == 'True':
+        time.sleep(5)
+        insight_text = 'This is a hardcoded test insight.'
+    else:
+        url = f"{BASE_ENDPOINT}/get_insight?entry={entry}&jprompt={jprompt}"
+        response = requests.request("GET", url)
 
-    insight_text = eval(response.text)['insight']
-    insight_text = insight_text.encode('raw_unicode_escape').decode('unicode_escape').encode('utf-16_BE','surrogatepass').decode('utf-16_BE')
+        insight_text = eval(response.text)['insight']
+        insight_text = insight_text.encode('raw_unicode_escape').decode(
+            'unicode_escape').encode('utf-16_BE', 'surrogatepass').decode('utf-16_BE')
+
     if not memoir:
         logger.info('memoir not found for memoir id: {0}'.format(memoir_id))
         return
-    
+
     insight = Insight.objects.filter(memoir=memoir, journaler=user).first()
-    insight = Insight(memoir=memoir, journaler=user, release_timestamp=timezone.now(), text=insight_text)
+    insight = Insight(memoir=memoir, journaler=user,
+                      release_timestamp=timezone.now(), text=insight_text)
     insight.save()
     logger.info('insight saved for memoir id: {0}'.format(memoir_id))
