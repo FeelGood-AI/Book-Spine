192420dfd4e30ea17fb91fd86fbb276953966323
Guzzler <sharangpai123@gmail.com>
2023-04-12 23:59:03 -0400

    add push notifications api

--- journal_ai/auth/urls.py
+++ journal_ai/auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserRecordView, acceptOnboardingForUser
+from .views import UserRecordView, acceptOnboardingForUser, setNotificationSettings
 from rest_framework.authtoken import views as AuthViews
 
 urlpatterns = [
     path('user/onboarding/', acceptOnboardingForUser, name='user_onboarding'),
+    path('user/notification-settings/', setNotificationSettings, name='notifications_settings'),
     path('user/', UserRecordView.as_view(), name='users'),
     path('token/', AuthViews.obtain_auth_token, name='token'),
 ] 
